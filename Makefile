3bf4a12556e4998f7759133a1c20d9d71dd2d761
abdellah karani <abdellahkarani@gamil.com>
2024-10-12 08:43:53 +0100

    .

--- Makefile
+++ Makefile
@@ -1,19 +1,20 @@
 NAME = main
 SRC = main.c
+OBJ = $(SRC:.c=.o)
 
 CC = gcc
 CFLAGS = -Imlx_linux -I/usr/include
 LDFLAGS = -Lmlx_linux -lmlx_Linux -lXext -lX11 -lm -lz
 
-
 $(NAME): $(OBJ)
-	$(CC) $(SRC) $(LDFLAGS) -o $(NAME) && ./$(NAME)
-
+	$(CC) $(OBJ) $(LDFLAGS) $(CFLAGS)  -o $(NAME)
+	rm -f $(OBJ)
 
 clean:
-	rm -f $(SRC)
+	rm -f $(OBJ)
 
 fclean: clean
 	rm -f $(NAME)
 
+re: fclean $(NAME)
 
--- main.c
+++ main.c
@@ -1,20 +1,13 @@
-#include <mlx.h>
+#include <pthread.h>
+#include <unistd.h>
 
-#define WIN_WIDTH 800
-#define WIN_HEIGHT 600
+
 
 int main(void) {
-    void *mlx;          // Pointer to the mlx instance
-    void *win;          // Pointer to the window
-
-    mlx = mlx_init();   // Initialize the MiniLibX
-    win = mlx_new_window(mlx, WIN_WIDTH, WIN_HEIGHT, "Simple MiniLibX Window"); // Create a new window
-
-    // Fill the window with a color (e.g., red)
-    mlx_clear_window(mlx, win); // Clear the window
-    mlx_pixel_put(mlx, win, WIN_WIDTH / 2, WIN_HEIGHT / 2, 0xFF0000); // Draw a red pixel in the center
-
-    mlx_loop(mlx); // Enter the event loop
+    
+	
+	    
+    
 
     return 0;
 }
