6232ecf65d3b7940dc868e576d62e4a5c0340965
abdellah karani <abdellahkarani@gamil.com>
2024-10-21 11:39:00 +0100

    .

--- julia.c
+++ julia.c
@@ -7,70 +7,14 @@
 #define HEIGHT 600 
 
 
-// NOTE: you need to handle each byte of the image to correctly display the pixel colors
-/*
-	// Define the Complex Constant:
-		Zn+1 = Zn^2 + C
-		cRe: real part of C
-		cIm: imaginary of C 
-	// 
-*/
 
-void *julia_fractl(void *mlx, void *win_ptr, void *image, int *data, int size_line, int bpp, int endian) {
 
-	// Loop over the screen
-	/*
-		size_line = 5440 = 1360 * 4
-			each pixel present by 4 byte (red, green, blue, alpha)
-	*/
-	printf("size_line = %d\n", size_line);
-	for (int x = 0; x < HEIGHT; x++) {
-		for (int y = 0; y < WIDTH; y++) {
-            
-			// Find pixel position on complex plane
-			double zx = 1.5 * (x - WIDTH / 2) / (0.5 * WIDTH) + 0;
-            double zy = (y - HEIGHT / 2) / (0.5 * HEIGHT) + 0;
 
- 			// Zn^2 + C (C = cRe + cIm * i)
-        	float cRe = 0.45, cIm = 0.1428;
-            
-			// Julia iteration
-        	int maxIterations = 100;
-        	int iteration = 0;
-            
-            /*
-            	zx: real part of of Zn
-            	zy: imaginary part of Zn
-            */
-			while (zx * zx + zy * zy < 4 && iteration < maxIterations) {
-		        double tmp = zx * zx - zy * zy + cRe;
-		        zy = 2 * zx * zy + cIm;
-		        zx = tmp;
-		        iteration++;
-        	}
-
-			/*
-				if (iteration == maxIterations):
-					point is part of julia set
-				else:
-					point outside the julia set
-			*/
-			if (iteration == maxIterations)   {
-				data[x * (size_line / 4) + y] = 0x000000;
-			} else {
-				int green = (iteration * 255 / maxIterations);
-    			int color = green << 8; // shift 8 for green color
-    			data[x * (size_line / 4) + y] = color;
-			}
-
-		}
-	}	
+	// make julia alone
 	
 	
 	
-	
-	return NULL;
-}
+
 
 
 int main() {
